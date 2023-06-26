9cc2b52de3f45774797c0b9a1857b26e17ff82e2
Cristian Ferney Caicedo Castillo <cfcaicedoca@unal.edu.co>
2023-06-26 10:02:27 -0500

    avance

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import * as AOS from 'aos';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
+  state = false;
   title = 'cv-cristian';
   private apiUrl = 'https://api.github.com/repos/Cristianc144/Sistema-almacenamiento-Registros';
   private apiUrl2 = 'https://api.github.com/repos/Cristianc144/Paginaventas-JavaScript-cursoPlatzi';
@@ -36,5 +36,39 @@ export class AppComponent {
   getRepositorio4() {
     return this.http.get(this.apiUrl4);
   }
+
+  scrollHaciaAbout() {
+    const about = document.getElementById('about');
+    if (about) {
+      about.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollHaciaSkills() {
+    const skills = document.getElementById('skills');
+    if (skills) {
+      skills.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollHaciaPortfolio() {
+    const portfolio = document.getElementById('portfolio');
+    if (portfolio) {
+      portfolio.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollHaciaHome() {
+    const portfolio = document.getElementById('home');
+    if (portfolio) {
+      portfolio.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+ changeStateAbout(){
+    this.state = true;
+    console.log(`this.state es: ${this.state}`)
+    }
+
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private sanitizer: DomSanitizer){}
+  downloadSpanishPDF() {
+    const filePath = '../assets/CV-Spanish.pdf';
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.target = '_blank';
+    link.download = 'CV-Cristian-ES.pdf'; // Puedes personalizar el nombre del archivo aquí
+  
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  downloadEnglishPDF() {
+    const filePath = '../assets/CV-English.pdf';
+    const link = document.createElement('a');
+    link.href = filePath;
+    link.target = '_blank';
+    link.download = 'CV-Cristian-EN.pdf'; // Puedes personalizar el nombre del archivo aquí
+  
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 
 }
